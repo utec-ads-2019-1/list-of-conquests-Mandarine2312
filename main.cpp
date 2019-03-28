6a1fa8bc3a3c188425d0ec7a561170761dc2c1ce
Alejandro <alejandro.tang@utec.edu.pe>
2019-03-28 11:59:19 -0500

    Primer commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
+int main() {
+    int num;
+    map<string, int> paises;
 
-int main(int argc, char *argv[]) {
+    string name;
+
+    cin >> num;
+    cin.ignore();
+
+    for(int i = 0; i < num; i++){
+        getline(cin, name, ' ');
+        cin.ignore(100, '\n');
+
+
+        if(paises.find(name) == paises.end()){
+            paises[name] = 1;
+        }else{
+            paises[name] = paises[name] + 1;
+        }
+    }
+
+
+    for(auto pais : paises){
+        cout << pais.first << " " << pais.second << endl;
+    }
+
+
     return 0;
 }
